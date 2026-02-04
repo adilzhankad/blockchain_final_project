91da16d8d61050bb08fa210e6dab66e6350eca3f
adilzhankadyrov <akomen1000@gmail.con>
2026-02-04 22:36:14 +0500

    add crowdfunding + contract deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,17 +3,19 @@ const hre = require("hardhat");
 async function main() {
   const Token = await hre.ethers.getContractFactory("RewardToken");
   const token = await Token.deploy();
-  await token.deployed();
-  console.log("Token deployed to:", token.address);
+  await token.waitForDeployment();
+  const tokenAddress = await token.getAddress();
+  console.log("Token deployed to:", tokenAddress);
 
-  const Crowd = await hre.ethers.getContractFactory("HumanitarianCrowdfunding");
-  const crowd = await Crowd.deploy(token.address);
-  await crowd.deployed();
-  console.log("Crowdfunding deployed to:", crowd.address);
+  const Crowd = await hre.ethers.getContractFactory("Crowdfunding"); 
+  const crowd = await Crowd.deploy(tokenAddress);
+  await crowd.waitForDeployment();
+  const crowdAddress = await crowd.getAddress();
+  console.log("Crowdfunding deployed to:", crowdAddress);
 
-  const tx = await token.setMinter(crowd.address);
+  const tx = await token.setMinter(crowdAddress);
   await tx.wait();
-  console.log("Minter set ");
+  console.log("Minter set");
 }
 
 main().catch((error) => {
