8b81b9eb9069b0aec1a1fd866ea6eb1cbf96f229
Yerulan <aidala.damyn@gmail.com>
2026-02-08 15:37:49 +0500

    Fixing bugs and connecting to local network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
-  solidity: "0.8.20",
+  defaultNetwork: "localhost", // <--- ДОБАВЬТЕ ЭТУ СТРОКУ
+  solidity: "0.8.28",
   networks: {
-    localhost: { url: "http://127.0.0.1:8545" },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+    },
   },
 };
